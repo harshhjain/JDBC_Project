a263cb562e9d00cec48cf9efccd610402977ec30
Harsh Vardhan Jain <66672238+harshhjain@users.noreply.github.com>
2022-06-12 21:46:47 +0530

    Update AppSettings.java

--- src/com/hvj/hvjproduct/AppSettings.java
+++ src/com/hvj/hvjproduct/AppSettings.java
@@ -2,8 +2,8 @@ package com.hvj.hvjproduct;
 
 public class AppSettings {
 
-    public static String UserName = "harsh@product1";
-    public static String PassWord = "Yash@2020";
+    public static String UserName = "harsh21@product121";
+    public static String PassWord = "Yash21@2020";
     public static String ConnectionString = "jdbc:sqlserver://product1.database.windows.net:1433;database=HVPROD;user=" + UserName +";password="+ PassWord + ";encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
 
 }
